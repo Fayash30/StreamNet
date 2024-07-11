bcb57e8fd2f5ebebc0fe70a90948ddc809e74de5
Fayash30 <mohamedfayash35@gmail.com>
2024-07-11 22:22:28 +0530

    Searching Functionality Implemented

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import {useState , useEffect} from 'react'
+import {Box ,Typography} from "@mui/material"
+import { Videos} from './'
+import { useParams } from 'react-router-dom';
+import { fetchFromAPI } from '../utils/API';
 
 const SearchFeed = () => {
+
+  const [videos, setVideos] = useState([]);
+  const { searchTerm } = useParams();
+  useEffect(() =>{
+      fetchFromAPI(`search?part=snippet&q=${searchTerm}`)
+      .then((data) => setVideos(data.items))
+      .catch((error) => console.error("Error setting videos:", error));
+  }, [searchTerm])
+
+
   return (
-    <div>
-      
-    </div>
+    <Box p={2} sx={{overflowY:'auto',height:'90vh',flex:2}}>
+    <Typography variant='h4' fontWeight="bold" mb={2} sx={{color:'white'}}>
+      Search Results for: <span style= {{color:"#f31503"}}>
+        {searchTerm}
+      </span>
+    </Typography>
+
+    <Videos videos={videos}/>
+  </Box>
   )
 }
 
-export default SearchFeed;
+export default SearchFeed
